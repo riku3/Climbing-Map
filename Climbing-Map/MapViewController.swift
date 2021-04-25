5f217e38f11bcf00cc2bd44741d9b7a5e960580a
riku3 <takarakuten62@gmail.com>
2021-04-25 13:50:23 +0900

    現在地取得

--- Climbing-Map/MapViewController.swift
+++ Climbing-Map/MapViewController.swift
@@ -6,13 +6,42 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // ロケーションマネージャーのセットアップ
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager!.requestWhenInUseAuthorization()
     }
+}
 
-
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        // 許可されてない場合
+        case .notDetermined:
+            // 許可を求める
+            manager.requestWhenInUseAuthorization()
+        // 拒否されてる場合
+        case .restricted, .denied:
+            // 何もしない
+            break
+        // 許可されている場合
+        case .authorizedAlways, .authorizedWhenInUse:
+            // 現在地の取得を開始
+            manager.startUpdatingLocation()
+            break
+        default:
+            break
+        }
+    }
 }
