a842c0269afc04ab1a3573d772fb5a3b1410fa90
riku3 <takarakuten62@gmail.com>
2021-04-25 13:17:06 +0900

    Mapクラス作成(不要なクラス削除)

---
+++ Climbing-Map/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  Climbing-Map
+//
+//  Created by riku on 2021/04/25.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
