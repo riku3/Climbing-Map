54f8b3663a6847acc9eaa22477cf50beaac06ca0
riku3 <takarakuten62@gmail.com>
2021-04-26 10:33:26 +0900

    tableview作成

--- Climbing-Map/MapDetailViewController.swift
+++ Climbing-Map/MapDetailViewController.swift
@@ -11,9 +11,47 @@ class MapDetailViewController: UIViewController {
     
     @IBOutlet weak var mapDetailTableView: UITableView!
     
+    enum Cell: Int, CaseIterable {
+            case imageCustomViewCell
+            case rockCustomViewCell
+            case projectCustomViewCell
+
+            var cellIdentifier: String {
+                switch self {
+                case .imageCustomViewCell: return "ImageTableViewCell"
+                case .rockCustomViewCell: return "RockTableViewCell"
+                case .projectCustomViewCell: return "ProjectTableViewCell"
+                }
+            }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        mapDetailTableView.register(UINib(nibName: "ImageTableViewCell", bundle: nil), forCellReuseIdentifier: "ImageTableViewCell")
+        mapDetailTableView.register(UINib(nibName: "RockTableViewCell", bundle: nil), forCellReuseIdentifier: "RockTableViewCell")
+        mapDetailTableView.register(UINib(nibName: "ProjectTableViewCell", bundle: nil), forCellReuseIdentifier: "ProjectTableViewCell")
+    }
+}
+
+extension MapDetailViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Cell.allCases.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellType = Cell(rawValue: indexPath.row)!
+        switch cellType {
+        case .imageCustomViewCell:
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellType.cellIdentifier) as! ImageTableViewCell
+            return cell
+        case .rockCustomViewCell:
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellType.cellIdentifier) as! RockTableViewCell
+            return cell
+        case .projectCustomViewCell:
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellType.cellIdentifier) as! ProjectTableViewCell
+            return cell
+        }
     }
 }
