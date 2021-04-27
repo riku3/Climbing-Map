986c793395a8b9a487601bb76302700e19a4a02a
riku3 <takarakuten62@gmail.com>
2021-04-27 11:55:57 +0900

    webview　戻るボタン作成

--- Climbing-Map/WKWebviewController.swift
+++ Climbing-Map/WKWebviewController.swift
@@ -26,5 +26,30 @@ class WKWebviewController: UIViewController {
             let request = URLRequest(url: url)
             webView.load(request)
         }
+        createBackButton()
+    }
+    
+    private func createBackButton() {
+        let backBtn = UIButton(type: .custom)
+        backBtn.frame = CGRect(x: self.view.frame.width-80, y: self.view.frame.height-180, width: 50, height: 50)
+        backBtn.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+        backBtn.tintColor = UIColor.black
+        backBtn.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        backBtn.layer.cornerRadius = 25
+        backBtn.layer.shadowColor = UIColor.black.cgColor
+        backBtn.layer.shadowRadius = 1
+        backBtn.layer.shadowOffset = CGSize(width: 1, height: 1)
+        backBtn.layer.shadowOpacity = 0.7
+
+        backBtn.addTarget(self,action: #selector(WKWebviewController.buttonTapped(_:)),for: .touchUpInside)
+        view.addSubview(backBtn)
+    }
+    
+    @objc func buttonTapped(_ sender : Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        } else {
+            dismiss(animated: true, completion: nil)
+        }
     }
 }
