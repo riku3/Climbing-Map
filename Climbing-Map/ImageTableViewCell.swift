d506e32a3c6e2b5ca1b0d762794365c4edd12ed0
riku3 <takarakuten62@gmail.com>
2021-04-29 20:06:45 +0900

    不要なコメント削除

--- Climbing-Map/ImageTableViewCell.swift
+++ Climbing-Map/ImageTableViewCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-//import FirebaseStorage
 
 class ImageTableViewCell: UITableViewCell {
 
@@ -23,33 +22,5 @@ class ImageTableViewCell: UITableViewCell {
     
     func setRock(name: String) {
         self.name.text = name
-        
-//        let storage = Storage.storage()
-//        let reference = storage.reference(forURL: "gs://sotoiwa-map.appspot.com/rocks/\(name)/\(name).jpg")
-//        reference.downloadURL { url, error in
-//            if let url = url {
-//                do {
-//                    let data = try Data(contentsOf: url)
-//                    self.rockImageView.image = UIImage(data: data)
-//                } catch let error {
-//                    print("Error : \(error.localizedDescription)")
-//                }
-//            } else {
-//                print(error ?? "")
-//            }
-//        }
-        
-//        let urlString = "https://storage.cloud.google.com/sotoiwa-map.appspot.com/rocks/\(name)/\(name).jpg?authuser=1"
-//        let urlString = "https://storage.cloud.google.com/sotoiwa-map.appspot.com/rocks/test/test.jpg?authuser=1"
-//        let urlString = "https://storage.cloud.google.com/sotoiwa-map.appspot.com/rocks/test/test.jpg"
-////        let encodeUrlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
-//        if let url = URL(string: urlString) {
-//            do {
-//               let data = try Data(contentsOf: url)
-//               self.rockImageView.image = UIImage(data: data)
-//             }catch let err {
-//               print("Error : \(err.localizedDescription)")
-//             }
-//        }
     }
 }
